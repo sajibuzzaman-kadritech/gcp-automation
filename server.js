a3924ba90a5fff9f728e5e9cdfd97acdcb72a3f8
sajibuzzaman-kadritech <sajib.uzzaman@kadritech.se>
2023-04-10 16:28:52 +0600

    create build.yml file for automation

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 5001
+const open = require('open');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', async (req, res) => {
+  console.log("click")
+  await open('https://sindresorhus.com');
 })
 
 app.listen(port, () => {
